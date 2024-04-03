f8c77c3809bc27d7345ba99f5d3535fa85079e48
ArunA-IT <arun20040619@gmail.com>
2024-04-03 15:37:01 +0530

    Tests

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
 from fastapi import FastAPI
+from fastapi import FastAPI,Request,Depends,Form
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import RedirectResponse,JSONResponse
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi import Form
+
+app = FastAPI()
+templates=Jinja2Templates(directory="templates")
+app.mount("/templates", StaticFiles(directory="templates"), name="templates")
+
+
+
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get('/', response_class=HTMLResponse)
+async def get_order(request: Request):
+    return templates.TemplateResponse("test.html", {"request": request})
